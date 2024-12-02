6ed5c63730856a491596f8ff46fd7cabe7d5d7dd
ArthitDev <rthit096@gmail.com>
2024-12-03 00:10:00 +0700

    feat : change api

--- src/Control/Service/Login.js
+++ src/Control/Service/Login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3123';
+const API_URL = import.meta.env.VITE_API_URL || 'https://api.tlgm.xyz';
 
 // Create axios instance with default config
 const api = axios.create({
